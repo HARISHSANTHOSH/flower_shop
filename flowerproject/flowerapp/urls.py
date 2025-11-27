dab57fcb3013cefbee02cb185c48b2bca1355fac
HARISHSANTHOSH <hkc3392@gmail.com>
2025-11-27 10:30:30 +0000

    implement new dashboard

--- flowerproject/flowerapp/urls.py
+++ flowerproject/flowerapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import FlowerListCreateAPIView, flower_page,login_page
+from .views import FlowerListCreateAPIView, flower_page,login_page,BuyNowAPIView
 
 
 urlpatterns = [
 	path('api/v1/flowers/', FlowerListCreateAPIView.as_view()),
 	path('flowers/', flower_page, name='flower_page'),
 	path('login/', login_page, name='login_page'),
+	path('api/buy-now/', BuyNowAPIView.as_view(), name='buy-now'),
 	
 ]
 
--- flowerproject/flowerapp/views.py
+++ flowerproject/flowerapp/views.py
@@ -16,19 +16,28 @@ from rest_framework.permissions import (
 
 class FlowerListCreateAPIView(APIView):
     permission_classes = [IsAdminUser]
-
+	
     def get(self, request):
         flowers = (
             models.Flower.objects.select_related("category").order_by("id")
         )
-
-        search = request.GET.get("search")
         category_id = request.GET.get("category")
+        price_range=request.GET.get('price')
+        if price_range:
+            try:
+                min_price,max_price=price_range.split("-")
+                min_price = int(min_price)
+                max_price=int(max_price)
 
-        if search:
-            flowers = flowers.filter(
-                Q(name__icontains=search) | Q(description__icontains=search)
-            )
+                if max_price:
+                    flowers=flowers.filter(price__gte=min_price,price__lte=max_price)
+                else:
+                    flowers=flowers.filter(price__gte=min_price)
+            except ValueError:
+                pass
+
+
+       
 
         if category_id:
             flowers = flowers.filter(category_id=category_id)
@@ -38,11 +47,11 @@ class FlowerListCreateAPIView(APIView):
         return paginator.get_paginated_response(serializer.data)
 
     def post(self, request):
-        serializer = serializers.FlowerSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            serializer = serializers.FlowerSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 def flower_page(request):
@@ -52,3 +61,29 @@ def flower_page(request):
 
 def login_page(request):
     return render(request, "login.html")
+
+
+class BuyNowAPIView(APIView):
+    def post(self,request):
+        user=request.user
+        customer, created = models.Customer.objects.get_or_create(user=user)
+        flower_ids=request.data.get("flowers",[])
+
+        if not flower_ids:
+            return Response("No flowers found")
+
+        order=models.Order.objects.create(customer=customer)
+        total=0
+
+        for fl_id in flower_ids:
+            flower=models.Flower.objects.get(id=fl_id)
+            item= models.OrderItem.objects.create(
+                order=order,
+                flower=flower,
+                quantity=1,
+                unit_price=flower.price
+            )
+            total +=item.get_total_price()
+        order.total_amount=total
+        order.save()
+        return Response({"order_id":order.id,"total":total,"status":order.status})
