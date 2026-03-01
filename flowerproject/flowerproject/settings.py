a0b0c00856b7bd733bb8c87fe682013491b7366d
HARISHSANTHOSH <hkc3392@gmail.com>
2026-03-01 19:39:01 +0000

    Fixx db

--- flowerproject/flowerproject/settings.py
+++ flowerproject/flowerproject/settings.py
@@ -110,11 +110,7 @@ DATABASES = {
     }
 }
 
-import dj_database_url
 
-DATABASES = {
-    'default': dj_database_url.parse(os.getenv('DATABASE_PUBLIC_URL'))
-}
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
